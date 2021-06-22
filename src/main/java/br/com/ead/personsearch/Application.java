d1ddb99fa77935f493365be9bb47f285217f1375
Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>
2021-06-22 17:12:45 -0300

    Adding first attempt to configure retries and circuit breakers

--- src/main/java/br/com/ead/personsearch/Application.java
+++ src/main/java/br/com/ead/personsearch/Application.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 @EnableConfigurationProperties
 @SpringBootConfiguration
--- src/main/java/br/com/ead/personsearch/services/PersonService.java
+++ src/main/java/br/com/ead/personsearch/services/PersonService.java
@@ -3,15 +3,32 @@ package br.com.ead.personsearch.services;
 import br.com.ead.personsearch.model.PersonEntity;
 import br.com.ead.personsearch.repositories.PersonRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.ElasticsearchException;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class PersonService {
 
+    CircuitBreakerFactory circuitBreakerFactory;
     PersonRepository repository;
 
+    @Retryable(value = ElasticsearchException.class, maxAttempts = 2, backoff = @Backoff(delay = 100))
     public PersonEntity save(PersonEntity person) {
-        return repository.save(person);
+        var circuitBreaker = circuitBreakerFactory.create("person-service-save");
+        return circuitBreaker.run(() -> repository.save(person));
+    }
+
+    @Recover
+    void recover(ElasticsearchException exception, PersonEntity person){
+        log.error("There was a error while saving the a person. " +
+                "The service will send this request to a external topic for further analysis. Person: {}, Exception: {}",
+                person, exception);
     }
 }
